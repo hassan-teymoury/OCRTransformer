ffddbac6c1ca0592a2af50562656576c3ff810e7
Hassan <hassan.teymouri@shab.ir>
2023-11-03 19:39:17 +0330

    pretrain Update 7

--- ocrtransform/model/trainer.py
+++ ocrtransform/model/trainer.py
@@ -96,7 +96,7 @@ class Train(object):
     def train(self):
         
         if not os.path.isdir(self.model_dir):
-            pretrained_path = "https://drive.google.com/drive/folders/14G7kcc3SLG7Jpeixp_VHmHnshmQNE7UB?usp=sharing"
+            pretrained_path = "https://drive.google.com/drive/folders/162ahxNB49qdcjfilWFIujEisoHN9BgTv?usp=sharing"
         
             print("Downloading t5 Seq2Seq pretrained model ....")
             gdown.download_folder(pretrained_path, quiet=True, use_cookies=False,
