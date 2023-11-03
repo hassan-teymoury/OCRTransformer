abddad9624186be131884f59357677e6d9d8baf2
Hassan <hassan.teymouri@shab.ir>
2023-11-03 19:37:12 +0330

    pretrained updates 6

--- ocrtransform/model/detector.py
+++ ocrtransform/model/detector.py
@@ -18,7 +18,7 @@ class Detector(object):
         # Downloading Pretrained weights
         self.model_dir = os.path.join(os.getcwd(), "checkpoints")
         if not os.path.isdir(self.model_dir):
-            pretrained_path = "https://drive.google.com/drive/folders/11aWKsqR1PAVrBoxa7wjj9Fem9Txr5qL-?usp=sharing"
+            pretrained_path = "https://drive.google.com/drive/folders/14G7kcc3SLG7Jpeixp_VHmHnshmQNE7UB?usp=sharing"
             
             print("Downloading t5 Seq2Seq pretrained model ....")
             gdown.download_folder(pretrained_path, quiet=True, use_cookies=False,
